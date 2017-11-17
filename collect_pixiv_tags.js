174baca40fed6d06d7ba4d094b456b5b6f996c9f
nekonekod <nekonekod.goo@gmail.com>
2017-11-17 23:54:21 +0800

    exist overide

--- collect_pixiv_tags.js
+++ collect_pixiv_tags.js
@@ -18,27 +18,29 @@
   db.defaults({ pixivs: [] }).write();
 
   var isIllustPage = window.location.href.indexOf('illust_id') > -1;
-  
+
   $(document).keydown(function (e) {
     if (e.shiftKey && e.keyCode === 68) {
       downLoadAllPixivData();
     }
   });
 
-  toast('','')
+  toast('', '')
 
-  if(isIllustPage){
+  if (isIllustPage) {
     //图片页
     var illustId = pixiv.context.illustId;
     saveForIllustPage(illustId); //直接触发
-    $('.js-nice-button').on('click', saveForIllustPage);
+    $('.js-nice-button').on('click', function () {
+      saveForIllustPage(illustId)
+    });
 
     $(document).keydown(function (e) {
       if (e.keyCode === 76) {
         saveForIllustPage(illustId);
       }
     });
-  }else{
+  } else {
     //作品页
     console.log('member page');
     saveForMemberPage();
@@ -47,15 +49,15 @@
   /**
    * 作品页的保存
    */
-  function saveForMemberPage(){
+  function saveForMemberPage() {
     var authorName = $('.profile').find('.user-name').text();
     var msgs = [];
-    $('._image-items').find('.image-item').each((i,li) => {
+    $('._image-items').find('.image-item').each((i, li) => {
       var $img = $(li).find('._layout-thumbnail img');
       var tags = $img.data('tags').split(' ');
       var illustId = "" + $img.data('id');
-      if(checkExist(illustId)){
-        console.log("Ignored:"+illustId);
+      if (removeExist(illustId)) {
+        console.log("Ignored:" + illustId);
         msgs.push('IGNORED:[' + illustId + ']');
       } else {
         var model = {
@@ -66,13 +68,13 @@
           author: authorName,
           authorId: $img.data('user-id'),
         };
-        if(save(model)){
-          console.log("SAVED:"+JSON.stringify(model));
+        if (save(model)) {
+          console.log("SAVED:" + JSON.stringify(model));
           msgs.push('SAVED:[' + model.id + ']');
         }
       }
     });
-    toast('' , JSON.stringify(msgs,undefined,2));
+    toast('', JSON.stringify(msgs, undefined, 2));
   }
 
   /**
@@ -80,14 +82,14 @@
    */
   function saveForIllustPage(illustId) {
     if (illustId) {
-      var exists = checkExist(illustId);
+      var exists = removeExist(illustId);
       if (exists) {
-          toast('IGNORED', JSON.stringify(exists,undefined, 2));
-        }else{
-          var model = infoForIllustPage(illustId);
-          if (model && save(model)) {
-            toast('SAVED' , JSON.stringify(model,undefined, 2));
-          }
+        toast('IGNORED', JSON.stringify(exists, undefined, 2));
+      } else {
+        var model = infoForIllustPage(illustId);
+        if (model && save(model)) {
+          toast('SAVED', JSON.stringify(model, undefined, 2));
+        }
       }
     }
   }
@@ -127,16 +129,21 @@
   }
 
   /**
-   * if exists return the value else return false
+   * if exists, delete orig
    */
-  function checkExist(illustId) {
-    var existed = db.get('pixivs').find({ id: illustId }).value();
+  function removeExist(illustId) {
+    var existed = db.get('pixivs').remove({ id: illustId }).value();
     if (existed) {
-      console.log('existed,so ingore');
-      return existed;
+      console.log('remove exist:' + JSON.stringify(existed));
     }
     return false;
   }
+
+  //处理脏数据
+  // var wrongs = db.get('pixivs').remove({ id: {
+  //   "isTrusted": true
+  // } }).value();
+  // console.log(wrongs)
 
   function save(model) {
     db.get('pixivs').push(model).write();
@@ -158,8 +165,11 @@
   }
 
   function downLoadAllPixivData() {
-    var data = db.get('pixivs').value();
-    downloadFile(new Date().toLocaleDateString() + '.json', JSON.stringify({pixiv:data} ,undefined, 2));
+    var pixivs = db.get('pixivs').value();
+    var data = JSON.stringify({ pixiv: pixivs }, undefined, 2);
+    downloadFile(new Date().toLocaleDateString() + '.json', data);
+    
+    // window.open('data:text/html;charset=utf-8;Base64,'+window.btoa('ssssdsaa'),'pixiv','width=200,height=100');
   }
 
   window.showAllPixivData = showAllPixivData;
@@ -179,39 +189,37 @@
   }
 
   // ******************** toast *******************//
-  function toast(header,body) {
+  function toast(header, body) {
     var $tip = $('#tip');
     if ($tip.length === 0) {
       $tip = $('<div id="tip" style="position:fixed;font-size:15px;padding:8px;top:10px;left: 10px;border:solid 0.5px #150101;background-color:#d6d6d6;z-index:9999"></div>');
       $tip.append($('<strong></strong>'));
 
 
-      var $bodyPre = $('<pre></pre>');
-      $bodyPre.on('click',()=>{
-        if($tip.css('left')){
-          $tip.css('left',undefined);
-          $tip.css('right','10px');
-        }else{
-          $tip.css('left','10px');
-          $tip.css('right',undefined);
-        }
-
-      });
-      $tip.append($bodyPre);
+      $tip.append($('<pre></pre>'));
 
       var $dlBtn = $('<button style="width: 75px;height: 25px;margin-right: 10px;">info</button>');
-      $dlBtn.off('click').on('click',downLoadAllPixivData);
+      $dlBtn.off('click').on('click', downLoadAllPixivData);
       $tip.append($dlBtn);
 
       var $closeBtn = $('<button style="width: 75px;height: 25px;margin-right: 10px;">close</button>');
-      $closeBtn.off('click').on('click',()=>{
+      $closeBtn.off('click').on('click', () => {
         $('#tip').remove();
       });
       $tip.append($closeBtn);
 
       $('body').append($tip);
     }
-    $tip.find('strong').text(header).show();
+    var $header = $tip.find('strong').text('<' + header + '>');
+    $header.off('click').on('click', () => {
+      if ($tip.css('left')) {
+        $tip.css('left', undefined);
+        $tip.css('right', '10px');
+      } else {
+        $tip.css('left', '10px');
+        $tip.css('right', undefined);
+      }
+    }).show();
     $tip.find('pre').text(body).show();
   }
 
